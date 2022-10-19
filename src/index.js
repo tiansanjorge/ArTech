07411c0873f97010df5de1d8f97217208f087ee6
tian sanjorge <tiansanjorge13@gmail.com>
2022-10-19 20:32:30 -0300

    pre-entrega1+Sanjorge

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 // import './styles/styles.scss';
 
-// import './index.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
